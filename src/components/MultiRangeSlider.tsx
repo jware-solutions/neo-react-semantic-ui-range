93542a856d59ee2736fe462b453df25559f2f70d
Apolo4444 <shizoudoragon@gmail.com>
2021-12-14 17:53:08 -0300

    feat: add jsdoc comments. add prittier. remove default react.js proyect files

--- src/components/MultiRangeSlider.tsx
+++ src/components/MultiRangeSlider.tsx
@@ -1,12 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
 import "../style/multiRangeSlider.css";
-import { RangeColor } from "../types";
+import { RangeColor } from "../types/range-colors";
 
-type updateCurrentValuesFunctionType = (
-  newActualMin: number,
-  newActualMax: number
-) => any;
+/**
+ * @param {number} newActualValue - Function to be invoked when the value of the slider changes
+ * @returns {any} - Return value defined in the function
+ */
+type updateCurrentValuesFunctionType = (newActualMin: number, newActualMax: number) => any;
 
+/**
+ * @param {number} defaultMin - Minimum value that can be setted on the slider
+ * @param {number} defaultMax - Maximum value that can be setted on the slider
+ * @param {number} minValue - Starting minium value setted on the slider
+ * @param {number} maxValue - Starting maximum value setted on the slider
+ * @param {number} [margin = 0] - Minimum difference between minimum and maximum values setted
+ * @param {RangeColor} [color = Green] - Color of the selected slice of the slider
+ * @param {boolean} [showLabels = false] - Indicates if minimum/maximum values labels should be shown
+ * @param {updateCurrentValuesFunctionType} onChange - Callback function that will be invoked when {actualValue} changes
+ */
 interface MultiRangeSliderProps {
   defaultMin: number;
   defaultMax: number;
@@ -18,44 +29,37 @@ interface MultiRangeSliderProps {
   onChange: updateCurrentValuesFunctionType;
 }
 
-/**
- *
- * @param props
- * @returns
- */
 const MultiRangeSlider = function (props: MultiRangeSliderProps) {
-  const {
-    defaultMin,
-    defaultMax,
-    minValue,
-    maxValue,
-    onChange,
-    margin = 0,
-    showLabels = false,
-    color = "Green",
-  } = props;
+  const { defaultMin, defaultMax, minValue, maxValue, onChange, margin = 0, showLabels = false, color = "Green" } = props;
 
-  // Convert to percentage
+  /**
+   * Returns the selected slice of the Slider, in percent from 0 to 100
+   *
+   * @param {number} value - New value setted on the slider
+   * @returns {number} - percent of the slider that need to be colored, from 0 to 100
+   */
   const getPercent = (value: number) => {
     return Math.round(((value - defaultMin) / (defaultMax - defaultMin)) * 100);
   };
 
   /**
-   * Handle Status Changes
+   * Invokes callback function if slider value is updated.
    *
-   * @param newMinValue new min value selected from the input.
-   * @param newMaxValue new max value selected from the input.
+   * @param {number} newMinValue - New minimum value setted on the slider
+   * @param {number} newMaxValue - New maximum value setted on the slider
    */
   const handleChange = (newMinValue: number, newMaxValue: number) => {
-    if (
-      newMinValue + margin > newMaxValue ||
-      newMaxValue - margin < newMinValue
-    ) {
+    if (newMinValue + margin > newMaxValue || newMaxValue - margin < newMinValue) {
       return;
     }
     onChange(newMinValue, newMaxValue);
   };
 
+  /**
+   * If {showLabels} is true, returns <div> that constains a labels with the current maximum and minimum values of the Slider
+   *
+   * @returns {HTMLElement} <div> with labels
+   */
   const renderLabels = () => {
     if (showLabels) {
       return (
@@ -96,10 +100,7 @@ const MultiRangeSlider = function (props: MultiRangeSliderProps) {
       <div className="slider">
         <div className="slider__track" />
         {renderLabels()}
-        <div
-          style={{ left: left, width: width, backgroundColor: color }}
-          className={"slider__range"}
-        />
+        <div style={{ left: left, width: width, backgroundColor: color }} className={"slider__range"} />
       </div>
     </>
   );
--- src/components/SingleRangeSlider.tsx
+++ src/components/SingleRangeSlider.tsx
@@ -1,8 +1,21 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { RangeColor } from "../types";
+import "../style/multiRangeSlider.css";
+import { RangeColor } from "../types/range-colors";
 
+/**
+ * @param {number} newActualValue - Function to be invoked when the value of the slider changes
+ * @returns {any} - Return value defined in the function
+ */
 type updateCurrentValueFunctionType = (newActualValue: number) => any;
 
+/**
+ * @param {number} defaultMinValue - Minimum value that can be setted on the slider
+ * @param {number} defaultMaxValue - Maximum value that can be setted on the slider
+ * @param {number} actualValue - Starting value setted on the slider
+ * @param {RangeColor} [color = Green] - Color of the selected slice of the slider
+ * @param {boolean} [showLabels = false] - Indicates if minimum/maximum values labels should be shown
+ * @param {updateCurrentValueFunctionType} onChange - Callback function that will be invoked when {actualValue} changes
+ */
 interface SingleRangeSliderProps {
   defaultMinValue: number;
   defaultMaxValue: number;
@@ -13,27 +26,36 @@ interface SingleRangeSliderProps {
 }
 
 const SingleRangeSlider = function (props: SingleRangeSliderProps) {
-  const {
-    defaultMinValue,
-    defaultMaxValue,
-    actualValue,
-    color = "Green",
-    showLabels = false,
-    onChange,
-  } = props;
+  const { defaultMinValue, defaultMaxValue, actualValue, color = "Green", showLabels = false, onChange } = props;
 
+  /**
+   * Invokes callback function if slider value is updated.
+   *
+   * @param {number} newValue - New value setted on the slider
+   */
   const handleValueChange = (newValue: number) => {
     if (newValue >= defaultMinValue && newValue <= defaultMaxValue) {
       onChange(newValue);
     }
   };
 
+  /**
+   * Returns the selected slice of the Slider, in percent from 0 to 100
+   *
+   * @param {number} newValue - New value setted on the slider
+   * @returns {number} - percent of the slider that need to be colored, from 0 to 100
+   */
   const getSelectedSliderPercent = (newValue: number) => {
     return (newValue / defaultMaxValue) * 100;
   };
 
   const selectedPercent = `${getSelectedSliderPercent(actualValue)}%`;
 
+  /**
+   * If {showLabels} is true, returns <div> that constains a label with the current value of the Slider
+   *
+   * @returns {HTMLElement} <div> with label
+   */
   const renderLabels = () => {
     if (showLabels) {
       return (
@@ -59,10 +81,7 @@ const SingleRangeSlider = function (props: SingleRangeSliderProps) {
       <div className="slider">
         <div className="slider__track" />
         {renderLabels()}
-        <div
-          style={{ width: selectedPercent, backgroundColor: color }}
-          className={"slider__range"}
-        />
+        <div style={{ width: selectedPercent, backgroundColor: color }} className={"slider__range"} />
       </div>
     </>
   );
