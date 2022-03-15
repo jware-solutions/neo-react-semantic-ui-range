ea5f1a4ac2aaf614896b74a9071a2bb14aaaf49d
Genarito <genarocamele@hotmail.com>
2022-03-15 16:03:30 -0300

    Added types to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export { SingleRangeSlider } from './components/SingleRangeSlider'
 export { MultiRangeSlider } from './components/MultiRangeSlider'
 
 export type { RangeColor } from './types'
+
+export type { SingleRangeSliderProps } from './components/SingleRangeSlider'
+
+export type { MultiRangeSliderProps } from './components/MultiRangeSlider'
